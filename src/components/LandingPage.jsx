820975e5b537926b5c88ae46d11181b63542decc
sirnobys <sirnobys@gmamil.com>
2023-06-28 18:24:10 +0000

    added testimonial section

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,6 +5,7 @@ import pc from "../images/geek.jpg"
 import smile from "../images/smile.png"
 import { Button } from '@mui/material';
 import { Slider, slides } from './carousel/Carousel';
+import { Testimonial } from './testimonial/Testimonial';
 
 export const LandingPage = (props) => {
     return (
@@ -16,7 +17,7 @@ export const LandingPage = (props) => {
                         <span className='leading'>We build the best product design & engineering teams</span>
                         <span className='end'>We build the best product design & engineering teams <img width={16} src={smile} /> </span>
                         <span>
-                            <Button className='btn button-disruption button-full-rounded' variant="contained">Learn more</Button>
+                            <Button className='btn button-welcoming button-full-rounded' variant="contained">Learn more</Button>
                         </span>
                     </div>
                     <span><img width={600} height={600} src={pc} /></span>
@@ -47,9 +48,24 @@ export const LandingPage = (props) => {
                     <span><img width={600} height={600} src={pc} /></span>
                 </div>
 
-                <div className='layout-y'>
-                    <span>Hire us for your next venture</span>
+                <div className='layout-y call-to-action'>
+                    <span className='text-grey'>Hire us for your next venture <img width={30} src={smile} /></span>
                     <Button className='btn button-disruption button-full-rounded'>Say Hi</Button>
+                </div>
+                <div className='clients'>
+                    <span>Trusted By</span>
+                    <span className='layout-x'>
+                        <span>logos</span>
+                        <span>logos</span>
+                        <span>logos</span>
+                        <span>logos</span>
+                    </span>
+                </div>
+                <div className='testimony'>
+                    <div className='intro text-grey'>What Do Our Clients Say About Us?</div>
+                    <span className=''>
+                        <Testimonial/>
+                    </span>
                 </div>
             </div>
         </div>
---
+++ src/components/testimonial/Testimonial.jsx
@@ -0,0 +1,101 @@
+import './Testimonial.scss'
+export const Testimonial = () => {
+    return (
+        <div className="main">
+            <div className="testimonials">
+                <input type="radio" name="testimonial" id="input-testimonial1"  defaultChecked/>
+                <input type="radio" name="testimonial" id="input-testimonial2" />
+                <input type="radio" name="testimonial" id="input-testimonial3" />
+                <input type="radio" name="testimonial" id="input-testimonial4" />
+                <div className="testimonials-inner">
+                    <div className="testimonial">
+                        <div className="testimonial-photo">
+                            <div className="photo-background"></div>
+                            <div className="photo-author"></div>
+                        </div>
+                        <div className="testimonial-text">
+                            <p>Wait a second... you're telling me this testimonials slider is powered solely by CSS? That's some next-level web wizardry! I'm sold! Give me that mind-blowing slider to my website!</p>
+                        </div>
+                        <div className="testimonial-author">Henry Schwengle</div>
+                    </div>
+                    <div className="testimonial">
+                        <div className="testimonial-photo">
+                            <div className="photo-background"></div>
+                            <div className="photo-author"></div>
+                        </div>
+                        <div className="testimonial-text">
+                            <p>The older I get, the more I understand why roosters just scream to start the day.</p>
+                        </div>
+                        <div className="testimonial-author">Mr. George Robert</div>
+                    </div>
+                    <div className="testimonial">
+                        <div className="testimonial-photo">
+                            <div className="photo-background"></div>
+                            <div className="photo-author"></div>
+                        </div>
+                        <div className="testimonial-text">
+                            <p>Two of my friends have never met each other. Before they spoke, I told both of them that the other is a bit deaf. They shouted at each other for a few minutes before they realized that I'm an asshole.</p>
+                        </div>
+                        <div className="testimonial-author">Doodle Wobblepants</div>
+                    </div>
+                    <div className="testimonial">
+                        <div className="testimonial-photo">
+                            <div className="photo-background"></div>
+                            <div className="photo-author"></div>
+                        </div>
+                        <div className="testimonial-text">
+                            <p>Turning on your lights and sirens after losing a drag race is just poor sportsmanship, man.</p>
+                        </div>
+                        <div className="testimonial-author">John "Vroom" Cena</div>
+                    </div>
+                </div>
+                <div className="testimonials-arrows">
+                    <div className="arrow arrow-left">
+                        <label htmlFor="input-testimonial1"></label>
+                        <label htmlFor="input-testimonial2"></label>
+                        <label htmlFor="input-testimonial3"></label>
+                        <label htmlFor="input-testimonial4"></label>
+                        <span></span>
+                    </div>
+                    <div className="arrow arrow-right">
+                        <label htmlFor="input-testimonial1"></label>
+                        <label htmlFor="input-testimonial2"></label>
+                        <label htmlFor="input-testimonial3"></label>
+                        <label htmlFor="input-testimonial4"></label>
+                        <span></span>
+                    </div>
+                </div>
+                <div className="testimonials-bullets">
+                    <label htmlFor="input-testimonial1">
+                        <div className="bullet">
+                            <div>
+                                <span></span>
+                            </div>
+                        </div>
+                    </label>
+                    <label htmlFor="input-testimonial2">
+                        <div className="bullet">
+                            <div>
+                                <span></span>
+                            </div>
+                        </div>
+                    </label>
+                    <label htmlFor="input-testimonial3">
+                        <div className="bullet">
+                            <div>
+                                <span></span>
+                            </div>
+                        </div>
+                    </label>
+                    <label htmlFor="input-testimonial4">
+                        <div className="bullet">
+                            <div>
+                                <span></span>
+                            </div>
+                        </div>
+                    </label>
+                </div>
+            </div>
+        </div>
+    )
+}
