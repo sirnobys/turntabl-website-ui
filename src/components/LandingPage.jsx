072c46c60f6227261e117203ccf302711c43dc65
sirnobys <sirnobys@gmamil.com>
2023-06-29 13:31:59 +0000

    added responsiveness

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,28 +3,38 @@ import { useNavigate } from 'react-router-dom';
 import { Nav } from './Nav';
 import pc from "../images/geek.jpg"
 import smile from "../images/smile.png"
-import { Button } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import { Slider, slides } from './carousel/Carousel';
 import { Testimonial } from './testimonial/Testimonial';
+import { useTheme } from '@mui/material/styles';
 
 export const LandingPage = (props) => {
+    const theme = useTheme();
+    console.log(theme.breakpoints.up('sm'));
     return (
         <div>
             <Nav />
             <div className='body'>
-                <div className='section'>
-                    <div className='text'>
-                        <span className='leading'>We build the best product design & engineering teams</span>
-                        <span className='end'>We build the best product design & engineering teams <img width={16} src={smile} /> </span>
-                        <span>
-                            <Button className='btn button-welcoming button-full-rounded' variant="contained">Learn more</Button>
-                        </span>
-                    </div>
-                    <span><img width={600} height={600} src={pc} /></span>
-                </div>
+                <Grid container spacing={2} alignItems={'center'}>
+                    <Grid item xs={12} lg={6}>
+                        <div className='section'>
+                            <div className='text'>
+                                <span className='leading'>We build the best product design & engineering teams</span>
+                                <span className='end'>We build the best product design & engineering teams <img width={16} src={smile} /> </span>
+                                <span>
+                                    <Button className='btn button-welcoming button-full-rounded' variant="contained">Learn more</Button>
+                                </span>
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <span><img width="100%" src={pc} /></span>
+                    </Grid>
+                </Grid>
+
                 <div className='slider'>
                     <div className='layout-x'>
-                        <span className='button-event-coming'><Button className='button' variant="text">Upcoming Events</Button></span>
+                        <span><Button className='btn button-welcoming button-partial-rounded' variant="text">Upcoming Events</Button></span>
                         <span><Button className='btn button-inspiration button-partial-rounded' variant="contained">Current Events</Button></span>
                         <span><Button className='btn button-insightful button-partial-rounded' variant="contained">Blogs</Button></span>
                     </div>
@@ -37,16 +47,22 @@ export const LandingPage = (props) => {
                     some text to motivate you
                 </div>
 
-                <div className='section'>
-                    <div className='text'>
-                        <span className='leading'>We build the best product design & engineering teams</span>
-                        <span className='end'>We build the best product design & engineering teams <img width={16} src={smile} /> </span>
-                        <span>
-                            <Button className='button' variant="contained">Learn more</Button>
-                        </span>
-                    </div>
-                    <span><img width={600} height={600} src={pc} /></span>
-                </div>
+                <Grid container spacing={2} alignItems={'center'}>
+                    <Grid item xs={12} lg={6}>
+                        <div className='section'>
+                            <div className='text'>
+                                <span className='leading'>We build the best product design & engineering teams</span>
+                                <span className='end'>We build the best product design & engineering teams <img width={16} src={smile} /> </span>
+                                <span>
+                                    <Button className='btn button-welcoming button-full-rounded' variant="contained">Learn more</Button>
+                                </span>
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <span><img width="100%" src={pc} /></span>
+                    </Grid>
+                </Grid>
 
                 <div className='layout-y call-to-action'>
                     <span className='text-grey'>Hire us for your next venture <img width={30} src={smile} /></span>
@@ -64,7 +80,7 @@ export const LandingPage = (props) => {
                 <div className='testimony'>
                     <div className='intro text-grey'>What Do Our Clients Say About Us?</div>
                     <span className=''>
-                        <Testimonial/>
+                        <Testimonial />
                     </span>
                 </div>
             </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,73 @@
 import React from 'react';
 import logo from "../images/logo.png"
+import useMediaQuery from '@mui/material/useMediaQuery';
 
-export const Nav = (props) => {
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export function BasicMenu() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <div>
+            <Button
+                id="basic-button"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+            >
+                <MenuIcon className='text-inspiration'/>
+            </Button>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <MenuItem onClick={handleClose}>Home</MenuItem>
+                <MenuItem onClick={handleClose}>About Us</MenuItem>
+                <MenuItem onClick={handleClose}>Clients</MenuItem>
+                <MenuItem onClick={handleClose}>Contact</MenuItem>
+            </Menu>
+        </div>
+    );
+}
+
+export const Nav = (props) => {
+    const matches = useMediaQuery('(min-width:800px)');
+    if (!matches) {
+        return (
             <div className='nav'>
-                <div className='logo'><img width={150} src={logo}/></div>
-                <div className='nav-link'>
-                    <span>Home</span>
-                    <span>About Us</span>
-                    <span>Clients</span>
-                    <span>Contact</span>
+                <div className='logo'><img width={150} src={logo} /></div>
+                <BasicMenu />
+            </div>
+        )
+    }
+    if (matches)
+        return (
+            <div>
+                <div className='nav'>
+                    <div className='logo'><img width={150} src={logo} /></div>
+                    <div className='nav-link'>
+                        <span>Home</span>
+                        <span>About Us</span>
+                        <span>Clients</span>
+                        <span>Contact</span>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
 }
