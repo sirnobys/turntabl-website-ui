71f16697b0e1ed4bac9486bb1ad725cb08118c52
shifu-kobby <samuelarmahappiah@gmail.com>
2023-07-03 22:40:44 +0000

    added career sections

--- src/components/Careers.jsx
+++ src/components/Careers.jsx
@@ -1,62 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Grid } from "@mui/material";
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
+import ListItemIcon from '@mui/material/ListItemIcon';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import LabelIcon from '@mui/icons-material/Label';
 import { motion } from "framer-motion"
+import AddIcon from '@mui/icons-material/Add';
 
 import { Preview } from ".";
 
 
 const Careers = () => {
+    const [selectedCareer, setSelectedCareer] = useState({});
+    const [open, setOpen] = useState(false);
+    const [load, setLoad] = useState(true);
+
+    let baseUrl = 'http://localhost:5000';
+
+    const handleCareerSelection = (career) => {
+        console.log(career)
+        setSelectedCareer(career);
+    }
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+
+    let careers = [
+        { name: 'QA Engineer', department: 'Engineering', description: 'desc..', requirements: ['3 years experience', 'Knowledge in Git'], responsibilities: ['Write and run tests', 'Migrate surce code'], technologies: ['Python', 'React'], salary: 'free' },
+        { name: 'Front End Developer', department: 'Engineering', description: 'desc..', requirements: ['3 years experience hbsvs sdhsd sdhcsd sdhbcsdddddddbbdbdbdbdbdbdbdbdbdbdbd sdhbcsdddddddbbdbdbdbdbdbdbdbdbdbdbd sdhbcsdddddddbbdbdbdbdbdbdbdbdbdbdbd', 'Knowledge in Git'], responsibilities: ['Write and run tests', 'Migrate surce code'], technologies: ['Python', 'React'], salary: 'free' },
+        { name: 'Backend Developer', department: 'Engineering', description: 'desc..', requirements: ['3 years experience', 'Knowledge in Git'], responsibilities: ['Write and run tests', 'Migrate surce code'], technologies: ['Python', 'React'], salary: 'free' },
+    ]
     return (
-        // <Box
-        //     sx={{
-        //         display: 'flex',
-        //         flexDirection: 'row',
-        //         alignItems: 'center',
-        //         gap: '10px'
-        //     }}
-        // >
-        <Grid container spacing={2} alignItems={'center'} paddingBottom={10}>
-            <Grid item xs={12} sm={12} md={3}>
-                <motion.div
-                    className="box"
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{
-                        duration: 0.8,
-                        delay: 0.5,
-                        ease: [0, 0.71, 0.2, 1.01]
-                    }}
-                >
-                    <Card sx={{ height: {xs: '250px', sm:'400px', md: '700px'} }}>
-                        <CardContent>
-                            <List>
-                                <ListItem>
-                                    <ListItemText>one</ListItemText>
-                                </ListItem>
-                                <ListItem>
-                                    <ListItemText>one</ListItemText>
-                                </ListItem>
-                            </List>
-                        </CardContent>
-                    </Card>
-                </motion.div>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                gap: '2px'
+            }}
+        >
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    width: '100%'
+                }}
+            >
+                <Typography sx={{ pb: 5, fontSize: { xs: '35px', sm: '40px' } }}>Careers</Typography>
+                <div>
+                    <Button
+                        sx={{
+                            fontSize: { xs: 8, sm: 12 },
+                            color: '#b0b0ff',
+                            borderColor: '#b0b0ff',
+                            ":hover": {
+                                color: '#6d6e71',
+                                borderColor: '#6d6e71',
+                            }
+                        }}
+                        variant="outlined"
+                        onClick={handleClickOpen}
+                        endIcon={<AddIcon />}>
+                        New Career
+                    </Button>
+                </div>
+            </Box>
+            <Grid container spacing={2} alignItems={'center'} paddingBottom={10}>
+                <Grid item xs={12} sm={12} md={3}>
+                    <motion.div
+                        className="box"
+                        initial={{ opacity: 0, scale: 0.5 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{
+                            duration: 0.8,
+                            delay: 0.5,
+                            ease: [0, 0.71, 0.2, 1.01]
+                        }}
+                    >
+                        <Card sx={{ height: { xs: '200px', sm: '350px', md: '650px' } }}>
+                            <CardContent
+                            sx={{
+                                overflow: 'auto'
+                            }}
+                            >
+                                <List>
+                                    {careers.map((career) => (
+                                        <ListItem disablePadding>
+                                            <ListItemButton
+                                                onClick={() => handleCareerSelection(career)}
+                                            >
+                                                <ListItemIcon>
+                                                    <LabelIcon sx={{ fontSize: { xs: '16px', sm: '20px' } }} />
+                                                </ListItemIcon>
+                                                <ListItemText primaryTypographyProps={{ fontSize: { xs: '12px', sm: '16px' } }} primary={career.name} />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    ))}
+                                </List>
+                            </CardContent>
+                        </Card>
+                    </motion.div>
+                </Grid>
+                <Grid item xs={12} sm={12} md={9}>
+                    <Preview career={selectedCareer} />
+                </Grid>
             </Grid>
-            <Grid item xs={12} sm={12} md={9}>
-                <Preview />
-            </Grid>
-        </Grid>
-        // </Box>
+        </Box>
     )
 }
 
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -6,17 +6,17 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { motion } from "framer-motion"
+import { CardHeader } from "@mui/material";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import LabelIcon from '@mui/icons-material/Label';
 
-const bull = (
-    <Box
-        component="span"
-        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
-        •
-    </Box>
-);
 
 const Preview = (props) => {
+    const { career } = props;
+    console.log(career)
     return (
         <motion.div
             className="box"
@@ -28,25 +28,149 @@ const Preview = (props) => {
                 ease: [0, 0.71, 0.2, 1.01]
             }}
         >
-            <Card sx={{ flexGrow: 1, height: {xs: '480px', sm: '600px', md: '700px'} }}>
-                <CardContent>
-                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        Word of the Day
+            <Card sx={{ flexGrow: 1, height: { xs: '455px', sm: '600px', md: '650px' } }}>
+                <CardHeader title={career.name} subheader={career.department} />
+                <CardContent
+                    sx={{
+                        height: { xs: '320px', sm: '465px', md: '510px' },
+                        overflow: 'auto',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-between'
+                    }}
+                >
+                    <Typography
+                        sx={{
+                            mb: 1.5,
+                            fontSize: { xs: '12px', sm: '14px' }
+                        }}
+                        color="text.secondary">
+                        {career?.description}
                     </Typography>
-                    <Typography variant="h5" component="div">
-                        be{bull}nev{bull}o{bull}lent
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        adjective
-                    </Typography>
-                    <Typography variant="body2">
-                        well meaning and kindly.
-                        <br />
-                        {'"a benevolent smile"'}
-                    </Typography>
+                    <Box>
+                        <Typography
+                            sx={{
+                                fontSize: { xs: '16px', sm: '20px' }
+                            }}
+                        >
+                            Requirements
+                        </Typography>
+                        <List>
+                            {career?.requirements?.map((requirement) => (
+                                <ListItem disablePadding>
+                                    <ListItemIcon>
+                                        <LabelIcon sx={{ fontSize: { xs: '16px', sm: '20px' } }} />
+                                    </ListItemIcon>
+                                    <ListItemText primaryTypographyProps={{ fontSize: { xs: '12px', sm: '16px' } }} primary={requirement} />
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Box>
+                    <Box>
+                        <Typography
+                            sx={{
+                                fontSize: { xs: '16px', sm: '20px' }
+                            }}
+                        >
+                            Responsibilities
+                        </Typography>
+                        <List>
+                            {career?.responsibilities?.map((responsibility) => (
+                                <ListItem disablePadding>
+                                    <ListItemIcon>
+                                        <LabelIcon sx={{ fontSize: { xs: '16px', sm: '20px' } }} />
+                                    </ListItemIcon>
+                                    <ListItemText primaryTypographyProps={{ fontSize: { xs: '12px', sm: '16px' } }} primary={responsibility} />
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            gap: '5px'
+                        }}
+                    >
+                        <Typography
+                            sx={{
+                                fontSize: { xs: '16px', sm: '20px' }
+                            }}
+                        >
+                            Technologies:
+                        </Typography>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                gap: '5px'
+                            }}
+                        >
+                            {career?.technologies?.map((technology) => (
+                                <Typography
+                                    sx={{
+                                        fontSize: { xs: '12px', sm: '16px' }
+                                    }}
+                                >
+                                    {technology}
+                                </Typography>
+                            ))}
+                        </Box>
+                    </Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            gap: '5px',
+                        }}
+                    >
+                        <Typography
+                            sx={{
+                                display: 'flex',
+                                fontSize: { xs: '16px', sm: '20px' }
+                            }}
+                            variant="h5"
+                        >
+                            Compensation:
+                        </Typography>
+                        <Typography
+                            sx={{
+                                fontSize: { xs: '12px', sm: '16px' }
+                            }}
+                        >{career?.salary}
+                        </Typography>
+                    </Box>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            gap: '5px',
+                        }}
+                    >
+                        <Typography
+                            sx={{
+                                display: 'flex',
+                                fontSize: { xs: '16px', sm: '20px' }
+                            }}
+                            variant="h5"
+                        >
+                            Date Created:
+                        </Typography>
+                        <Typography
+                            sx={{
+                                fontSize: { xs: '12px', sm: '16px' }
+                            }}
+                        >{career?.date_created}
+                        </Typography>
+                    </Box>
                 </CardContent>
                 <CardActions>
-                    <Button size="small">Learn More</Button>
+                    <Button size="small" variant="contained">Edit</Button>
+                    <Button size="small" variant="contained">Delete</Button>
                 </CardActions>
             </Card>
         </motion.div>
