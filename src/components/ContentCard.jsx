5e077a043d0e19a1608da1450d4573937615255e
shifu-kobby <samuelarmahappiah@gmail.com>
2023-07-03 10:25:58 +0000

    added animations and dialog form for events

--- src/components/ContentCard.jsx
+++ src/components/ContentCard.jsx
@@ -5,54 +5,66 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { motion } from "framer-motion"
 
 
 const ContentCard = (props) => {
     const { id, name, description, image, status, links } = props;
     console.log(id, name, description, image, status, links)
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                component="img"
-                alt="green iguana"
-                height="140"
-                image={image}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {description} Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button
-                    variant="contained"
-                    size="small"
-                    sx={{
-                        backgroundColor: '#6d6e71',
-                        ":hover": {
-                            backgroundColor: '#b0b0ff',
-                        }
-                    }}>Share</Button>
-                {links.map((link, idx) => (
+        <motion.div
+            className="box"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                duration: 0.8,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01]
+            }}
+        >
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    component="img"
+                    alt="green iguana"
+                    height="140"
+                    image={image}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {description} Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+                <CardActions>
                     <Button
                         variant="contained"
-                        key={idx}
                         size="small"
                         sx={{
                             backgroundColor: '#6d6e71',
                             ":hover": {
                                 backgroundColor: '#b0b0ff',
                             }
-                        }}>
-                        <a style={{ textDecoration: 'none', color: '#fff' }} href={link}>Learn More</a>
-                    </Button>
-                ))}
-            </CardActions>
-        </Card>
+                        }}>Share</Button>
+                    {links.map((link, idx) => (
+                        <Button
+                            variant="contained"
+                            key={idx}
+                            size="small"
+                            sx={{
+                                backgroundColor: '#6d6e71',
+                                ":hover": {
+                                    backgroundColor: '#b0b0ff',
+                                }
+                            }}>
+                            <a style={{ textDecoration: 'none', color: '#fff' }} href={link}>{link.title}</a>
+                        </Button>
+                    ))}
+                </CardActions>
+            </Card>
+        </motion.div>
     );
 }
 
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,12 +1,46 @@
+import React, { useState } from "react";
 import { Box, Typography } from "@mui/material";
-import React from "react";
 import AddIcon from '@mui/icons-material/Add';
-import ContentCard from "./ContentCard";
 import { Button, Grid } from '@mui/material';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Textarea from '@mui/joy/Textarea';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 import { images } from "../constants";
+import ContentCard from "./ContentCard";
+
 
 const Events = () => {
+    const [open, setOpen] = useState(false);
+    const [status, setStatus] = useState('current');
+    const [linksCounter, setLinksCounter] = useState(['1']);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        setLinksCounter([1])
+    };
+
+    const handleStatusChange = (event) => {
+        setStatus((event.target).value);
+    };
+
+    const handleLinksCounter = (event) => {
+        setLinksCounter([...linksCounter, event.target.value])
+    }
+
     let events = [
         { id: 1, name: 'event1', description: 'desc', image: images.citi, status: 'UPCOMING', links: [{ title: 'title1', link: 'url' }] },
         { id: 2, name: 'event2', description: 'desc', image: images.people, status: 'CURRENT', links: [{ title: 'title2', link: 'url' }] },
@@ -25,7 +59,138 @@ const Events = () => {
                 }}
             >
                 <Typography sx={{ pb: 5, fontSize: { xs: '35px', sm: '40px' } }}>Events</Typography>
-                <Button sx={{ fontSize: { xs: 8, sm: 12 }, backgroundColor: '#b0b0ff' }} variant="contained" endIcon={<AddIcon />}>New Event</Button>
+                <div>
+                    <Button
+                        sx={{
+                            fontSize: { xs: 8, sm: 12 },
+                            color: '#b0b0ff',
+                            borderColor: '#b0b0ff',
+                            ":hover": {
+                                color: '#6d6e71',
+                                borderColor: '#6d6e71',
+                            }
+                        }}
+                        variant="outlined"
+                        onClick={handleClickOpen}
+                        endIcon={<AddIcon />}>
+                        New Event
+                    </Button>
+                    <Dialog open={open} onClose={handleClose}>
+                        <DialogTitle>Add New Event</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                                Fill in the details for new event
+                            </DialogContentText>
+                            <Box
+                                component="form"
+                                sx={{
+
+                                }}
+                                noValidate
+                                autoComplete="off"
+                            >
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="name"
+                                    label="Event Name"
+                                    type="text"
+                                    fullWidth
+                                    variant="outlined"
+                                />
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="image"
+                                    // label="Add Image"
+                                    type="file"
+                                    fullWidth
+                                    variant="outlined"
+                                />
+                                <FormControl>
+                                    <FormLabel sx={{ color: 'black' }}>Status</FormLabel>
+                                    <RadioGroup
+                                        aria-labelledby="demo-controlled-radio-buttons-group"
+                                        name="controlled-radio-buttons-group"
+                                        value={status}
+                                        onChange={handleStatusChange}
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'row'
+                                        }}
+                                    >
+                                        <FormControlLabel value="current" control={<Radio />} label="Current" />
+                                        <FormControlLabel value="upcoming" control={<Radio />} label="Upcoming" />
+                                    </RadioGroup>
+                                </FormControl>
+                                <Box>
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'row',
+                                            alignItems: 'center',
+                                            gap: '10px',
+                                            fontSize: '10px'
+                                        }}
+                                    >
+                                        <Typography>Links</Typography>
+                                        <div>
+                                            <Button
+                                                sx={{
+                                                    backgroundColor: '#b0b0ff',
+                                                    ":hover": {
+                                                        backgroundColor: '#6d6e71',
+                                                    }
+                                                }}
+                                                variant="contained" onClick={handleLinksCounter}>Add</Button>
+                                        </div>
+                                    </Box>
+                                    {linksCounter.map((item) => (
+                                        <Box
+                                            sx={{
+                                                display: 'flex',
+                                                flexDirection: 'row',
+                                                gap: '5px'
+                                            }}
+                                        >
+                                            <TextField
+                                                autoFocus
+                                                margin="dense"
+                                                id="link-name"
+                                                label="Name"
+                                                type="text"
+                                                fullWidth
+                                                variant="outlined"
+                                            />
+                                            <TextField
+                                                autoFocus
+                                                margin="dense"
+                                                id="link-url"
+                                                label="Url"
+                                                type="text"
+                                                fullWidth
+                                                variant="outlined"
+                                            />
+                                        </Box>
+                                    ))}
+                                </Box>
+                                <Textarea
+                                    sx={{
+                                        mt: 1
+                                    }}
+                                    placeholder="Event Description..."
+                                    minRows={3}
+                                    variant="outlined"
+                                    size="md"
+                                />
+                            </Box>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose}>Cancel</Button>
+                            <Button onClick={handleClose}>Submit</Button>
+                        </DialogActions>
+                    </Dialog>
+                </div>
             </Box>
             <Grid container spacing={2} alignItems={'center'} paddingBottom={10}>
                 {events.map((event, idx) => (
