2022ad505470f31c3315a60fc3f95a3d9df29ecd
sirnobys <sirnobys@gmamil.com>
2023-07-04 09:23:04 +0000

    added banner for about us

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
-import { Box, Typography } from "@mui/material"
+import { Box, Divider, Grid, Typography } from "@mui/material"
 
-export const Banner = ({ image, page, intro }) => {
+export const Banner = ({ bgImage, image, page, intro }) => {
 
     return (
         <Box className="banner" sx={{
-            background: `url(${image})`,
+            background: `url(${bgImage})`,
             backgroundSize: 'cover',
-            boxShadow: "inset 0 0 0 2000px rgba(0, 0, 0, 0.8)"
+            // backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundAttachment: 'fixed',
+            boxShadow: "inset 0 0 0 2000px rgba(255,255,255, 0.1)"
         }}>
-            <div >
-                <Typography color={'white'} sx={{ pb: 5, fontSize: { xs: '70px', sm: '100px', } }}>{page}</Typography>
-                <Typography color={'white'} sx={{ pb: 5, fontSize: { xs: '20px', sm: '30px', } }}>{intro}</Typography>
-            </div>
+            <Grid container spacing={2} alignItems={'center'}  sx={{paddingY:10}}>
+                <Grid item xs={12} lg={6} bgcolor={'rgba(255,255,255, 0.9)'}>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', paddingBottom:18 }}>
+                        <Typography className='mondwest' sx={{ fontSize: { xs: '70px', sm: '100px', } }}>{page}</Typography>
+                        <Divider sx={{ width: '80%', backgroundColor: 'black', p: 1 }} />
+                        <Typography className='mondwest' sx={{ px: 5, pt: 3, fontSize: { xs: '20px', sm: '30px', } }}>{intro}</Typography>
+                    </div>
+                </Grid>
+                <Grid item xs={12} lg={6} >
+                    {/* <img style={{ borderRadius: '50%' }} width={400}  src={image} /> */}
+                </Grid>
+            </Grid>
         </Box>
     )
 }
--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -2,15 +2,39 @@ import React from 'react';
 import { Footer, Nav } from '../components';
 import { Banner } from '../components/Banner';
 import { images } from '../constants';
+import { Grid, Typography } from '@mui/material';
 
 export const AboutUs = () => {
-const intro = "We Provide Innovative Software Engineering Solutions To BusinessesAll Over The World."
+    const intro = "We Provide Innovative Software Engineering Solutions To Businesses All Over The World."
     return (
         <div id='page-container'>
             <Nav />
             <div id='content-wrap'>
-                <Banner image={images.people} page={'About Us'} intro={intro} />
-        
+                <Banner bgImage={images.IMG_9005} image={images.pc} page={'About Us'} intro={intro} />
+                <Grid container spacing={0} alignItems={'center'}>
+                    <Grid item xs={12} lg={12}>
+                        <div className='text-clear-white mission' style={{ fontSize: '30px', padding: '80px', background: 'black' }}>
+                            <div className='layout-x'><span className="text-insightful" style={{ fontSize: "40px" }}>MISSION</span><img width="70px" src={images.thumb} /></div>
+                            <Typography className='mondwest' sx={{ fontSize: { xs: '30px', sm: '30px', } }}>
+                                Our mission is to make a difference in the world of digital technology and more
+                                specifically, in Ghana and the emerging skills of a digitally enabled workforce, and the
+                                world.
+                            </Typography>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} lg={12}>
+                        <div className='mission' style={{ fontSize: '30px', padding: '80px' }}>
+                            <div className='layout-x'><span style={{ fontSize: "40px" }}>We Are Ghana’s
+                                Technology
+                                Leaders</span><img width="70px" src={images.thumb} /></div>
+                            <Typography className='mondwest' sx={{ fontSize: { xs: '30px', sm: '30px', } }}>
+                                We provide your company with the best engineering services.
+                                Here, the most talented and creative experts work with the world’s leading organizations on initiatives that promote social change.
+                                We thrive on advancing global collaboration, developing software, and overcoming obstacles.
+                            </Typography>
+                        </div>
+                    </Grid>
+                </Grid>
             </div>
             <Footer />
         </div>
