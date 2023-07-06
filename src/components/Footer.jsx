e1fc04ce368e8df35cabfb045cd154e0848697d2
sirnobys <sirnobys@gmamil.com>
2023-07-06 10:01:20 +0000

    added links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Button, Divider, Grid, TextField } from '@mui/material';
+import { Button, Divider, Grid, Link, TextField } from '@mui/material';
 import React from 'react';
 import { Twitter, YouTube, LinkedIn } from '@mui/icons-material';
 import SendIcon from '@mui/icons-material/Send';
@@ -6,15 +6,17 @@ import SendIcon from '@mui/icons-material/Send';
 import { images } from '../constants';
 import { links } from '../constants';
 import { IconButton } from '@mui/joy';
+import { useNavigate } from 'react-router';
 
 
 const Footer = () => {
+    const navigate = useNavigate()
     return (
         <div id='footer'>
             <div className="footer-ceil">
                 <Grid container spacing={2} >
-                    <Grid item xs={12} md={4} lg={4}>
-                        <span><img width="50%" src={images.logo} alt='turntabl logo'/></span>
+                    <Grid item xs={12} md={4} lg={4} pb={2}>
+                        <span className='logo' onClick={() => navigate('/')}><img width="50%" src={images.logo} alt='turntabl logo' /></span>
                         <div className='news-letter'>
                             <span className='layout-x'>
                                 <TextField placeholder='enter email' size='small' label="Subscribe to news letter" />
@@ -23,13 +25,17 @@ const Footer = () => {
                         </div>
 
                     </Grid>
-                    <Grid item xs={6} sm={6} md={3} lg={3}>
+                    <Grid item xs={6} sm={6} md={3} lg={3} >
                         <span className='text-grey'>Company</span>
                         <div className="layout-y">
-                            <span>Home</span>
-                            <span>About Us</span>
-                            <span>Client</span>
-                            <span>Contact</span>
+                            <Grid item xs={12} sm={12} md={6} lg={6}>
+                                <Button variant="text" className="text-pixel-black" onClick={() => [navigate('/')]} > <Link className="text-grey" underline='none'>Home</Link></Button>
+                                <Button variant="text" className="text-pixel-black" onClick={() => [navigate('/about-us')]} > <Link className="text-grey" underline='none'>About Us</Link></Button>
+                                <Button variant="text" className="text-pixel-black" onClick={() => [navigate('/services')]} > <Link className="text-grey" underline='none'>Services</Link></Button>
+                                <Button variant="text" className="text-pixel-black" onClick={() => [navigate('/careers')]} > <Link className="text-grey" underline='none'>Careers</Link></Button>
+                                <Button variant="text" className="text-pixel-black" onClick={() => [navigate('/events')]} > <Link className="text-grey" underline='none'>Events</Link></Button>
+                                <Button variant="text" className="text-pixel-black" onClick={() => [navigate('/blog')]} > <Link className="text-grey" underline='none'>Blog</Link></Button>
+                            </Grid>
                         </div>
                     </Grid>
                     <Grid item xs={6} sm={6} md={2} lg={2}>
@@ -37,19 +43,19 @@ const Footer = () => {
                         <div className='social-media'>
                             <div className="layout-y">
                                 <span className='layout-x'>
-                                    <IconButton onClick={()=> window.open(links.YOUTUBE,'_blank')} size="xs" variant="plain" color="neutral">
+                                    <IconButton onClick={() => window.open(links.YOUTUBE, '_blank')} size="xs" variant="plain" color="neutral">
                                         <YouTube className='icons' />&nbsp;
                                         <span>Youtube</span>
                                     </IconButton>
                                 </span>
                                 <span className='layout-x'>
-                                    <IconButton onClick={()=> window.open(links.TWITTER,'_blank')} size="xs" variant="plain" color="neutral">
+                                    <IconButton onClick={() => window.open(links.TWITTER, '_blank')} size="xs" variant="plain" color="neutral">
                                         <Twitter className='icons' />&nbsp;
                                         <span>Twitter</span>
                                     </IconButton>
                                 </span>
                                 <span className='layout-x'>
-                                    <IconButton onClick={()=> window.open(links.LINKEDIN,'_blank')} size="xs" variant="plain" color="neutral">
+                                    <IconButton onClick={() => window.open(links.LINKEDIN, '_blank')} size="xs" variant="plain" color="neutral">
                                         <LinkedIn className='icons' />&nbsp;
                                         <span>LinkedIn</span>
                                     </IconButton>
