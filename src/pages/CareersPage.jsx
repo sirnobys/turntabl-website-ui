76344cf1990f2b055bf152fd6713bd325e9c5c9d
sirnobys <sirnobys@gmamil.com>
2023-07-07 09:08:57 +0000

    added search and filter for roles

--- src/pages/CareersPage.jsx
+++ src/pages/CareersPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Footer, Nav } from '../components';
 import { Banner } from '../components/Banner';
 import { images } from '../constants';
@@ -10,7 +10,25 @@ import HandshakeIcon from '@mui/icons-material/Handshake';
 import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 import SearchIcon from '@mui/icons-material/Search';
 import BusinessIcon from '@mui/icons-material/Business';
+
 export const CareersPage = () => {
+    const departmentData = [
+        { name: 'All' },
+        { name: 'Engineering' },
+        { name: 'IT Operations' },
+        { name: 'Operations' },
+        { name: 'Operations & Admin' },
+    ]
+    const [department, setDepartment] = useState('All')
+    const Filters = () => {
+        return (
+            <Grid container spacing={0} textAlign={'left'}>
+                {departmentData.map((dep, idx) => (<Grid item xs={4} sm={2}>
+                    <Chip key={idx} icon={<BusinessIcon />} variant={dep.name === department ? 'filled' : 'outlined'} label={dep.name} onClick={() => { setDepartment(dep.name) }} />
+                </Grid>))}
+            </Grid>
+        )
+    }
     const Roles = ({ title, description, department, resp }) => {
         return (
             <div>
@@ -35,7 +53,7 @@ export const CareersPage = () => {
                         resp:
                     </Grid>
                     <Grid item xs={12} lg={12}>
-                        <Chip icon={<BusinessIcon />} variant="outlined" label="Chip Filled" />
+                        <Chip icon={<BusinessIcon />} variant="outlined" label="Engineering" />
                     </Grid>
                 </Grid>
             </div>
@@ -47,12 +65,7 @@ export const CareersPage = () => {
         { value: 'Delivery for Client', icon: <CheckCircleOutlineIcon sx={{ color: '#bdff00' }} /> },
         { value: 'Accountability', icon: <HandshakeIcon sx={{ color: '#bdff00' }} /> }
     ]
-    const team = [
-        { name: 'Sam Moorhouse', bio: "", role: "CEO", image: "", twitter: "", linkedIn: "" },
-        { name: 'Inusah Ibrahim', bio: "", role: "Admin", image: "", twitter: "", linkedIn: "" },
-        { name: 'Sam Appiah', bio: "", role: "Engineer", image: "", twitter: "", linkedIn: "" },
-        { name: 'Mike Annan', bio: "", role: "Engineer", image: "", twitter: "", linkedIn: "" },
-    ]
+
     const intro = () => <span>  Be part of our Mission! <br />We are looking for passionate people to join us on our mission</span>
     return (
         <div id='page-container'>
@@ -60,7 +73,7 @@ export const CareersPage = () => {
             <div id='content-wrap'>
                 <Banner bgImage={images.IMG_9005} image={images.pc} page={'Careers'} intro={intro()} />
                 <Box paddingY={{ xs: 5, sm: 10 }} paddingX={{ xs: 5, sm: 10 }}>
-
+                    <Filters />
                     <Roles title={''} description={''} resp={''} department={''} />
                 </Box>
 
