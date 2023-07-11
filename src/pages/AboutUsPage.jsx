b351ebf439784ca4c314eab1574584e28bd20e6c
shifu-kobby <samuelarmahappiah@gmail.com>
2023-07-11 22:16:24 +0000

    updated who we are section animations

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -9,6 +9,7 @@ import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import HandshakeIcon from '@mui/icons-material/Handshake';
 import Team from '../components/Team';
+import { motion } from "framer-motion"
 
 export const AboutUs = () => {
     useEffect(() => {
@@ -35,61 +36,78 @@ export const AboutUs = () => {
                 <Banner bgImage={images.happy_faces} image={images.pc} page={'About Us'} intro={intro()} />
                 <Grid container spacing={0} alignItems={'center'}>
                     <Grid item xs={12} lg={12}>
-                        <Box className='mission' sx={{ px: { xs: 2, sm: 10 }, py: { xs: 2, sm: 10 }, }}>
-
-                            <div className='layout-x'>
+                        <motion.div
+                            whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+                            transition={{ duration: 0.5 }}
+                            className='layout-x'
+                        >
+                            <Box className='mission' sx={{ px: { xs: 2, sm: 10 }, py: { xs: 2, sm: 10 }, }}>
                                 <Typography className='alt-header-font' >We Are Ghana's
                                     Technology Leaders
                                 </Typography>
                                 {/* <img width="60px" src={images.thumb} /> */}
-                            </div>
-                            <Typography className='body-font' >
-                                Turntabl is a technical partner that combines decades of Fortune 500 technology and training experience with a track record in Ghana. We provide highly motivated engineers managed by staff seasoned in western companies. We are based in London and Accra, Ghana.
-                            </Typography>
-                        </Box>
+                                <Typography className='body-font' >
+                                    Turntabl is a technical partner that combines decades of Fortune 500 technology and training experience with a track record in Ghana. We provide highly motivated engineers managed by staff seasoned in western companies. We are based in London and Accra, Ghana.
+                                </Typography>
+                            </Box>
+                        </motion.div>
                     </Grid>
                     <Grid item xs={12} lg={12}>
                         <Box className='text-clear-white mission' sx={{ px: { xs: 2, sm: 10 }, py: { xs: 2, sm: 10 }, background: 'black' }}>
-                            <div className='layout-x'>
-                                <Typography className='alt-header-font text-insightful'>
-                                    Mission
+                            <motion.div
+                                whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+                                transition={{ duration: 0.5 }}
+                            >
+                                <div className='layout-x'>
+                                    <Typography className='alt-header-font text-insightful'>
+                                        Mission
+                                    </Typography>
+                                    <img width="70px" src={images.thumb} alt='thumb' />
+                                </div>
+                                <Typography className='body-font' sx={{ fontSize: { xs: '16px', sm: '16px', } }}>
+                                    Our mission is to make a difference in the world of digital technology and more
+                                    specifically, in Ghana and the emerging skills of a digitally enabled workforce, and the
+                                    world. <br />We are building an organisation that gives people high-quality learning, applied skills and
+                                    a fulfilling career and to create a sustainable enterprise that people admire and respect.
                                 </Typography>
-                                <img width="70px" src={images.thumb} alt='thumb' />
-                            </div>
-                            <Typography className='body-font' sx={{ fontSize: { xs: '16px', sm: '16px', } }}>
-                                Our mission is to make a difference in the world of digital technology and more
-                                specifically, in Ghana and the emerging skills of a digitally enabled workforce, and the
-                                world. <br />We are building an organisation that gives people high-quality learning, applied skills and
-                                a fulfilling career and to create a sustainable enterprise that people admire and respect.
-                            </Typography>
+                            </motion.div>
                         </Box>
                     </Grid>
                     <Grid item xs={12} lg={12}>
                         <Box className='mission' sx={{ px: { xs: 2, sm: 10 }, py: { xs: 2, sm: 10 }, }}>
-
-                            <div className='layout-x'>
-                                <Typography className='alt-header-font' >We Are Taking The World of Technology By Storm
+                            <motion.div
+                                whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+                                transition={{ duration: 0.5 }}
+                            >
+                                <div className='layout-x'>
+                                    <Typography className='alt-header-font' >We Are Taking The World of Technology By Storm
+                                    </Typography>
+                                    {/* <img width="60px" src={images.thumb} /> */}
+                                </div>
+                                <Typography className='body-font' >
+                                    We provide your company with the best engineering services.
+                                    Here, the most talented and creative experts work with the world's leading organizations on initiatives that promote social change.
+                                    We thrive on advancing global collaboration, developing software, and overcoming obstacles.
                                 </Typography>
-                                {/* <img width="60px" src={images.thumb} /> */}
-                            </div>
-                            <Typography className='body-font' >
-                                We provide your company with the best engineering services.
-                                Here, the most talented and creative experts work with the world's leading organizations on initiatives that promote social change.
-                                We thrive on advancing global collaboration, developing software, and overcoming obstacles.
-                            </Typography>
+                            </motion.div>
                         </Box>
                     </Grid>
                     <Grid item xs={12} lg={12}>
                         <Box className='text-clear-white mission' sx={{ px: { xs: 2, sm: 10 }, py: { xs: 2, sm: 10 }, background: 'black' }}>
-                            <div className='layout-x'>
-                                <Typography className='alt-header-font text-insightful'>
-                                    We Are Making A Difference
+                            <motion.div
+                                whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+                                transition={{ duration: 0.5 }}
+                            >
+                                <div className='layout-x'>
+                                    <Typography className='alt-header-font text-insightful'>
+                                        We Are Making A Difference
+                                    </Typography>
+                                    <img width="70px" src={images.thumb} alt='thumb' />
+                                </div>
+                                <Typography className='body-font' sx={{ fontSize: { xs: '16px', sm: '16px', } }}>
+                                    We have set up Turntabl to make a difference in Ghana. It is our aim to develop a sustainable enterprise with a quality focus and a global outlook. We aim for Turntabl to provide all the employees, including ourselves, with a good wage. At the same time we want to make as much positive change as possible. Ultimately we intend to part with the company, giving it away so that its wealth creation can benefit Ghana to the fullest.
                                 </Typography>
-                                <img width="70px" src={images.thumb} alt='thumb' />
-                            </div>
-                            <Typography className='body-font' sx={{ fontSize: { xs: '16px', sm: '16px', } }}>
-                            We have set up Turntabl to make a difference in Ghana. It is our aim to develop a sustainable enterprise with a quality focus and a global outlook. We aim for Turntabl to provide all the employees, including ourselves, with a good wage. At the same time we want to make as much positive change as possible. Ultimately we intend to part with the company, giving it away so that its wealth creation can benefit Ghana to the fullest.
-                            </Typography>
+                            </motion.div>
                         </Box>
                     </Grid>
                     <Grid item xs={12} lg={12} paddingTop={3}>
@@ -98,9 +116,14 @@ export const AboutUs = () => {
                     {
                         coreValues.map((e, idx) => (
                             <Grid key={idx} item xs={12} sm={6} md={3} paddingTop={4}>
-                                <div align="center">
+                                <motion.div
+                                    whileInView={{ opacity: 1 }}
+                                    whileHover={{ scale: 1.1 }}
+                                    transition={{ duration: 0.5, type: 'tween' }}
+                                    align="center"
+                                >
                                     <CoreValues value={e.value} icon={e.icon} />
-                                </div>
+                                </motion.div>
                             </Grid>
                         ))
                     }
@@ -110,9 +133,14 @@ export const AboutUs = () => {
                     {
                         team.map((e, idx) => (
                             <Grid key={idx} item xs={12} sm={6} md={3} paddingTop={4}>
-                                <div align="center">
+                                <motion.div
+                                    whileInView={{ opacity: 1 }}
+                                    whileHover={{ scale: 1.1 }}
+                                    transition={{ duration: 0.5, type: 'tween' }}
+                                    align="center"
+                                >
                                     <Team name={e.name} image={e.image} role={e.role} bio={e.bio} linkedIn={e.linkedIn} twitter={e.twitter} />
-                                </div>
+                                </motion.div>
                             </Grid>
                         ))
                     }
